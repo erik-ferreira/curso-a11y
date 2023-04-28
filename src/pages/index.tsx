19512c3b66661e93e6c02eace532e32da641f6b0
erik-ferreira <erikdesouzagf@gmail.com>
2023-04-28 17:01:52 -0300

    add landmarks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,10 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <div className={styles.header}>
+      <header className={styles.header}>
         <Image src={logoImg} width={286 / 2} />
 
-        <div className={styles.nav}>
+        <nav className={styles.nav}>
           <a href="https://github.com/erik-ferreira">
             <svg
               stroke="currentColor"
@@ -27,37 +27,49 @@ export default function Home() {
               />
             </svg>
           </a>
-        </div>
-      </div>
+        </nav>
+      </header>
 
-      <div className={styles.content}>
-        <h2>Desenvolvendo uma web acessível</h2>
+      <main>
+        <article className={styles.content}>
+          <header>
+            <h2>Desenvolvendo uma web acessível</h2>
 
-        <h4>
-          Protocolos e diretrizes orientam o desenvolvimento de tecnologias
-          acessíveis, mas é preciso olhar para além de tudo isso
-        </h4>
+            <h4>
+              Protocolos e diretrizes orientam o desenvolvimento de tecnologias
+              acessíveis, mas é preciso olhar para além de tudo isso
+            </h4>
+          </header>
 
-        <p>
-          Acessibilidade se tornou uma tendência no ecossistema tecnológico
-          mundial, diversas empresas passaram a adotar critérios de
-          desenvolvimento acessível em seus projetos — por uma questão prática
-          ou por exigências legais —, no entanto, ainda faltam recursos e
-          consciência do que de fato é inclusivo na internet para pessoas com
-          deficiências.
-        </p>
+          <p>
+            Acessibilidade se tornou uma tendência no ecossistema tecnológico
+            mundial, diversas empresas passaram a adotar critérios de
+            desenvolvimento acessível em seus projetos — por uma questão prática
+            ou por exigências legais —, no entanto, ainda faltam recursos e
+            consciência do que de fato é inclusivo na internet para pessoas com
+            deficiências.
+          </p>
 
-        <p>
-          Para abordar este tema, é preciso ter uma perspectiva cultural sobre o
-          assunto, não encará-lo apenas na ótica técnica ou prática.
-          Acessibilidade passou a ser vista como um desafio (ou tendência) para
-          o ecossistema tech e não como filosofia natural daquilo que condiz com
-          os princípios básicos da web: acessível para humanos. Tal afirmação
-          está na W3C.
-        </p>
+          <p>
+            Para abordar este tema, é preciso ter uma perspectiva cultural sobre
+            o assunto, não encará-lo apenas na ótica técnica ou prática.
+            Acessibilidade passou a ser vista como um desafio (ou tendência)
+            para o ecossistema tech e não como filosofia natural daquilo que
+            condiz com os princípios básicos da web: acessível para humanos. Tal
+            afirmação está na W3C.
+          </p>
 
-        <h3>O que é acessibilidade, afinal?</h3>
-      </div>
+          <h3>O que é acessibilidade, afinal?</h3>
+        </article>
+      </main>
+
+      <footer className={styles.header}>
+        <Image src={logoImg} width={286 / 2} />
+
+        <nav className={styles.nav} aria-label="Rodapé">
+          <a href="https://github.com/erik-ferreira">Termos de uso</a>
+        </nav>
+      </footer>
     </div>
   );
 }
